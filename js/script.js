7bde74d668fe622c4451fadb57f7ed2c55ee6424
Cezar <cezarlantosgmail.com>
2021-12-14 17:29:43 +0100

    esercizio terminato

---
+++ js/script.js
@@ -0,0 +1,54 @@
+const headLines = ['Topolino', 'Marvel', 'Disnep', 'Monster Allergy', 'Kung Fu Panda', 'Inazuma Eleven'];
+const paragraphs = [
+    'Topolino, ovvero Mickey Mouse, è un personaggio immaginario dei fumetti e dei cartoni animati creato il 16 gennaio 1928 da Walt Disney e Ub Iwerks e successivamente sviluppato da Floyd Gottfredson. Il debutto cinematografico del personaggio avvenne il 18 novembre 1928 nel cortometraggio Steamboat Willie. Nei fumetti il personaggio debuttò sui quotidiani due anni dopo, il 13 gennaio 1930 con la storia Topolino nell’isola misteriosa. Per la sua creazione Walt Disney vinse un Premio Oscar onorario.  Topolino è un topo antropomorfo che veste pantaloni rossi, grandi scarpe gialle e guanti bianchi. Walt Disney dichiarò in un’intervista che l’idea di Mickey Mouse arriva proprio dal suo ufficio e da un topolino domestico che veniva nel suo ufficio presso il Laugh-O-Gram Studio. In merito a questo evento egli stesso dichiarò: “Erano soliti lottare per le briciole nel mio cestino dei rifiuti, quando lavoravo da solo fino a tarda notte. Li presi e li tenni in gabbiette sulla mia scrivania. Mi affezionai particolarmente a un topo domestico marrone. Era un piccoletto timido. Toccandolo sul naso con la matita, lo addestrai a correre all’interno di un cerchio nero che avevo tracciato sul mio tavolo. Quando me ne andai da Kansas City per tentare la fortuna a Hollywood, mi dispiacque lasciarlo. Così lo portai in un cortile, facendo attenzione che fosse un bel quartiere, e il piccoletto domato corse verso la libertà.”',
+    'Tutto comincia nel 1942, quando Teschio Rosso trova in Norvegia il Tesseract, uno strano manufatto che contiene una delle Gemme dell\'Infinito, quella dello spazio. Il capo dell\'Hydra però deve fare i conti con il primo Avenger, ossia Captain America, pronto a tutto per riuscire a salvare il mondo. A sacrificarsi però non sarà solo Cap, ma anche il suo miglior amico, Bucky (Captain America, Il primo Vendicatore). Niente paura però: l\'Hydra recupera ciò che rimane del fedele compagno di Steve Rogers e lo trasforma nel Winter Soldier, il sicario che, qualche anno dopo, ucciderà i genitori di Tony Stark.',
+    '"Tutto è cominciato con un topo", era la frase che Disney ripeteva più spesso nelle sue interviste. In realtà, prima di creare Topolino nel 1928, Walt aveva già firmato parecchi disegni animati: i Laugh-O-Grams (1923), per esempio, o Alice in Cartoonland (1923), una serie in cui comparve per la prima volta il cattivo Gambadilegno che risulta così essere il suo personaggio più longevo. Nessuna di queste serie aveva però ottenuto molto successo, e un altro personaggio destinato a diventare abbastanza famoso, Osvaldo Leprotto (1927), gli era stato sottratto per questioni legali.',
+    'Elena Patata si è appena trasferita assieme ai genitori a Oldmill Village, nella periferia dell’immaginaria Bigburg. Nonostante il consiglio dei suoi coetanei, Elena si interessa al suo vicino di casa, Ezechiele Zick, detto Zick. Zick è all’apparenza un normale ragazzo di 10 anni, solitario e che soffre di svariate allergie, ma in realtà riesce anche a vedere i mostri e i fantasmi essendo difatti un Domatore, un mostro dalla curiosa forma umana in grado di usare particolari poteri per poter assoggettare i mostri e gli spettri. Nella sua casa, con la quale convive con la madre Greta Barrymore, una Rifugiatrice (un umano che vede i mostri), vi abitano diversi mostri, esiliati dalla loro città natale per alcuni crimini (quali rubare, creare disordine, diffondere menzogne e disturbare la quiete) e a controllarli c’è un Tutore, individui che fungono da amministratori dell’ordine nel mondo dei mostri. Tale tutore, Timothy-Moth, ha le sembianze di un gatto per non dover ricorrere all’invisibilità. Elena, seppur senza poter vedere i mostri, crede a ciò che sostiene Zick. Le loro avventure cominciano con la ricerca dei loro gatti, catturati dai Gorka, una mostruosa specie di mostri mutaforma che vogliono vendicarsi del loro esilio contro i Tutori, cercandoli nella loro forma gattesca.',
+    'Po è un panda obeso e maldestro che sogna (da una vita) di diventare un guerriero abile nell’arte del kung fu. Ma la realtà è ben diversa: Po lavora come cameriere nel ristorante di spaghetti di famiglia, gestito dal padre adottivo Ping. Intanto nel Palazzo di Giada, dimora del maestro Shifu e di Oogway, si allenano cinque guerrieri potentissimi: Tigre, Scimmia, Gru, Vipera e Mantide. Uno di loro, conosciuti come i ‘Cinque Cicloni’, sarà scelto dal saggio Oogway come detentore della mitica Pergamena del Drago, capace di elargire formidabili capacità a chiunque la legga. Frattanto l’anziano maestro Oogway ha fatto una visione: si tratta del ritorno dello spietato leopardo Tai Lung, ex allievo di Shifu; la cerimonia che avverrà a breve serve proprio a nominare il guerriero che sarà in grado di sconfiggerlo (dopo aver letto la Pergamena del Drago). La liturgia inizia e tutti gli abitanti accorrono nel Palazzo di Giada: ovviamente anche Po si reca alla festa ma non arriva in tempo. Per raggiungere la piazza centrale, Po escogita uno stratagemma con dei fuochi d’artificio che lo catapultano proprio al cospetto di Oogway: quest’ultimo nomina Po come Guerriero Dragone, interpretando la sua caduta dal cielo come un segno. Po è ancora incredulo ma accetta il gravoso ruolo. Il panda però non ne sa niente di Kung fu: lo aspetta un durissimo allenamento. Purtroppo però, come previsto da Oogway, il feroce Tai Lung riesce davvero ad evadere di prigione..',
+    'Inazuma Eleven è un anime basato sul calcio, un calcio molto diverso da quello reale. Ciò che rende speciale questa serie sono le super tecniche. Il protagonista della serie si chiama Mark Evans, capitano della Raimon. Nella prima stagione vediamo la nascita della squadra protagonista della serie, la Raimon, la squadra del club di calcio dell\' omonima scuola. Questa squadra ha come obbiettivo vincere il campionato nazionale del giappone, il Football Frontier. La seconda stagione invece inizia subito dopo la fine della prima, quando mentre la Raimon torna vittoriosa alla scuola, notano 3 sfere nere nel cielo che piano piano scendono fino a schiantarsi, creando una gran paura tra i ragazzi, dato che quelle 3 sfere si erano schiantate al suolo come un meteorite.. Quelle 3 sfere erano palloni da calcio, appartenenti ad alieni. Questi alieni sono calciatori che vengono dal pianeta Alius, e si fanno chiamare Alius Academy. In seguito ad una partita disastrosa per la Raimon, avendo molti giocatori infortunati, quest\' ultima dovrà girare il giappone e reclutare i migliori giocatori della nazione al fine di sconfiggere per sempre gli alieni. La terza serie è ambientato 3 mesi dopo la sconfitta dell\' Alius Academy e tutto è tornato alla normalità. Un giorno tutti i ragazzi della Raimon ricevono una chiamata dal loro allenatore, che li convoca alla scuola per dar loro una grande notizia. Essi parteciperanno alla prima edizione del campionato mondiale di calcio, il Football Frontier International (o FFI).'
+];
+const images = [
+    'https://gossipcomeeracomee.altervista.org/wp-content/uploads/2017/06/io_topolino-960x1316.png',
+    'https://i1.wp.com/www.cinefilos.it/wp-content/uploads/2014/03/marvel-1.jpg?fit=580%2C300&ssl=1',
+    'https://i0.wp.com/capitalgaming.mx/wp-content/uploads/2020/02/Asi-queda-el-calendario-de-estrenos-de-Disney.jpg?zoom=3&resize=150%2C150&ssl=1',
+    'https://th.bing.com/th/id/R.37fd3b8f98e92268ace8b0998ed5f8cc?rik=%2f4Pn3FYG8vXcgQ&riu=http%3a%2f%2fvignette2.wikia.nocookie.net%2fdoblaje%2fimages%2fc%2fc4%2fMonster_Allergy.jpg%2frevision%2flatest%3fcb%3d20100511001508%26path-prefix%3des&ehk=GQtICyZpRvHDXPd77MOip4wBgcNYGQ2LL5Q0JH64izo%3d&risl=&pid=ImgRaw&r=0',
+    'https://th.bing.com/th/id/R.1bf3bc8cad3af4266d9aec485341a5e6?rik=YoJ6Q7R3F2q2xQ&riu=http%3a%2f%2fmoplathebig.weebly.com%2fuploads%2f1%2f3%2f3%2f2%2f133220944%2f690604943_orig.jpg&ehk=25F4r%2fAAfjvo3oz230pkmTyppYB4zi0zo%2b%2bOc2OEC6A%3d&risl=&pid=ImgRaw&r=0',
+    'https://th.bing.com/th/id/R.188c4e850c21299771eb2a8611034996?rik=sQ5O9sOBG6HyOw&riu=http%3a%2f%2fimages.nintendolife.com%2freviews%2fwii%2finazuma_eleven_strikers%2f1280x720.jpg&ehk=50CfEn%2b6nBnzFEBnZLQpNXuv7Q5liIjYFUqvq0Eu0cU%3d&risl=&pid=ImgRaw&r=0'
+];
+
+let currentIndex = 0;
+
+const testata = document.querySelector('.titolo');
+const testo = document.querySelector('.paragrafo');
+const immagine = document.querySelector('.foto');
+const buttonLeft = document.querySelector('#button-left');
+const buttonRight = document.querySelector('#button-right');
+
+testata.innerText = 'Cartone vincitore 2021:'+ ' ' + headLines[currentIndex];
+testo.innerText = paragraphs[currentIndex];
+immagine.src = images[currentIndex];
+
+buttonRight.addEventListener('click', function() {
+    currentIndex++;
+    if(currentIndex > headLines.length -1) {
+        currentIndex = 1;
+    }
+    testata.innerText = 'Voto alternativo'+ ' ' + currentIndex + ': ' + headLines[currentIndex];
+    testo.innerText = paragraphs[currentIndex];
+    immagine.src = images[currentIndex];
+
+});
+
+buttonLeft.addEventListener('click', function() {
+    currentIndex--;
+    if(currentIndex < 1) {
+        currentIndex = 5;
+    }
+    testata.innerText = 'Voto alternativo'+ ' ' + currentIndex + ': ' + headLines[currentIndex];
+    testo.innerText = paragraphs[currentIndex];
+    immagine.src = images[currentIndex];
+});
+
+
+
+
